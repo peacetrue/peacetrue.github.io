4a2a9c2dc192acdea85e6d4d9a06919115b9568c
peacetrue <peacetrue@users.noreply.github.com>
2022-08-13 01:54:08 +0000

    Deploying to gh-pages from @ peacetrue/peacetrue.github.io@9eb13ba6e93446ac51f6b32236b6838983533080 🚀

---
+++ peacetrue-template/_attachments/templates/quick-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#mkdir quick-start && cd quick-start || exit
+#ln -s ../template-engine.jar template-engine.jar
+#ln -s ../antora antora
+#ln -s ../antora-options.properties antora-options.properties
+#ln -s ../antora-variables.properties antora-variables.properties
+#cd ..
+#zip -rq quick-start.zip quick-start
+
+rm -rf quick-start.zip
+zip -rq quick-start.zip quick-start
